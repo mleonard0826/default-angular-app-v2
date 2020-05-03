b59ebb30f9f65b77deb6c3f034840951430dc627
Michael Leonard <MichaelA.Leonard@LibertyMutual.com>
2020-05-03 00:09:57 -0400

    Attempts for Protractor

---
+++ protractorLaunchFile.js
@@ -0,0 +1,19 @@
+const protractorFlake = require('protractor-flake');
+const AgentJasmine = require('agent-js-jasmine');
+const reportportalConfig = require('./reportportalConf');
+const agent = new AgentJasmine(reportportalConfig);
+ 
+agent.getLaunchStartPromise().then((launchData) =>{
+    protractorFlake({
+        maxAttempts: 1,
+        protractorArgs: [
+            './multiThreadConf.js',
+            '--params.id',
+            launchData.id
+        ]
+    }, (status) => {
+        agent.getExitPromise().then(() =>{
+            process.exit(status);
+        });
+    });
+});
